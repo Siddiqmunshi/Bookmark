f7a7c781928f6a635cc7a8d566f01ccfa6e8d157
Siddiq Rahman <sidq@hotmail.co.uk>
2022-02-08 09:41:43 +0000

    Set up complete

---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,8 @@
+# in spec/features/viewing_bookmarks_spec.rb
+
+feature 'Viewing bookmarks' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Bookmark Manager"
+  end
+end
