e02fe1ed66510da3f93e2aa20ca21a3aba60268b
Siddiq Rahman <sidq@hotmail.co.uk>
2022-02-08 17:17:55 +0000

    Viewing bookmarks complete

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,19 @@
 # in spec/features/viewing_bookmarks_spec.rb
 
+# feature 'Viewing bookmarks' do
+#   scenario 'visiting the index page' do
+#     visit('/')
+#     expect(page).to have_content "Bookmark Manager"
+#   end
+# end
+# in spec/features/viewing_bookmarks_spec.rb
+
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
